e34cfe96b74b7191bf8e37ed57a6f39dca27dcb2
Jair <jairaceves56@gmail.com>
2023-11-24 13:51:26 -0600

    Mostramos la Fecha

---
+++ src/components/Clima.vue
@@ -0,0 +1,26 @@
+<script setup>
+    import useClima from '../composables/useClima';
+    const { formatearTemp } = useClima()
+
+    defineProps({
+        clima: {
+            type: Object,
+            required: true
+        }
+    })
+
+</script>
+
+
+
+<template>
+    <div class="resultado">
+        <h2 class="text-5xl font-black p-12">Clima de: {{ clima.name }}</h2>
+        <p class="actual">{{ formatearTemp(clima.main.temp) }}&deg;C</p>
+        <div class="temperaturas">
+            <p>Min: {{ formatearTemp(clima.main.temp_min) }}<span>&deg;C</span></p>
+            <p>Max: {{ formatearTemp(clima.main.temp_max) }}<span>&deg;C</span></p>
+        </div>
+    </div>
+</template>
+
